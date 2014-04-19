9ad27247a012bab0a1da5020c46467fcb72f0add
lizc577 <liz@lizcohen.net>
2014-04-18 21:35:55 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nomster::Application.config.secret_token = 'f7bbc88dc13d1cd6b92f2836c69eec04b781db341cdae4ef53a96a87f89a5e9be779537b338f1c2bfedc0a197e86dce061206096fc3fd3c2e1863099d2526cb9'
